ed9e0b791d12acddc138b3c844ca2d5f3d21c4f3
aigulbakieva <bakisha.a@mail.ru>
2023-08-08 21:57:17 +0100

    created str_func

--- str_func.py
+++ str_func.py
@@ -1,5 +1,5 @@
 def capital_letter(s):
-    """The function for return string with all capital letters"""
+    """The function for return string with all capital letters. """
     words = s.split()
     if len(words) > 0:
         words[0] = words[0][0].upper() + words[0][1:]
@@ -7,3 +7,9 @@ def capital_letter(s):
         if words[i - 1][1] in ".!?":
             words[i] = words[i][0].upper() + words[i][1:]
     return "".join(words)
+
+
+def upper_first_letter(s):
+    """The function for capitalizes the first letters of each word in a string. """
+    words = s.split()
+    return words[0].upper()
